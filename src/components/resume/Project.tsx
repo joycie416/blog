cdd1505bcd459dbbc23eebf177a8300bbfece9a6
joycie416 <joycie416@gmail.com>
2024-12-18 18:03:44 +0900

    css: 프로젝트 카드 css 수정

--- src/components/resume/Project.tsx
+++ src/components/resume/Project.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
-import TakkomImg from "../../public/projects/takkom.svg"
+import TakkomImg from "../../public/projects/takkom.svg";
 
 const Project = () => {
   return (
     <section className="w-full sm:w-full sm:max-w-[--max-width] flex flex-col p-4 mx-auto sm:px-0">
       <h3>Projects</h3>
-      <div className="grid grid-cols-3 gap-6">
+      <div className="grid sm:grid-cols-3 gap-4 sm:gap-6">
         <ProjectCard
-          data={{ name: "따꼼", description: "우리 아이 예방 접종 기록을 저장하고 동네 병원을 찾아보세요.", image: TakkomImg }}
+          data={{
+            name: "따꼼",
+            description:
+              "우리 아이 예방 접종 기록을 저장하고 동네 병원을 찾아보세요.",
+            image: TakkomImg,
+          }}
         />
         <ProjectCard
-          data={{ name: "레코드 팡팡", description: "우리 아이 예방 접종 기록을 저장하고 동네 병원을 찾아보세요.", image: TakkomImg }}
+          data={{
+            name: "레코드 팡팡",
+            description:
+              "우리 아이 예방 접종 기록을 저장하고 동네 병원을 찾아보세요.",
+            image: "",
+          }}
         />
       </div>
     </section>
--- src/components/resume/ProjectCard.tsx
+++ src/components/resume/ProjectCard.tsx
@@ -1,22 +1,34 @@
-import Image from 'next/image';
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 type ProjectCardProps = {
   name: string;
   description: string;
   image: string;
-}
+};
 
-const ProjectCard = ({data:{name, description, image}}: {data: ProjectCardProps}) => {
+const ProjectCard = ({
+  data: { name, description, image },
+}: {
+  data: ProjectCardProps;
+}) => {
   return (
-    <div className='w-full h-[400px] flex flex-col border rounded-xl overflow-hidden transition hover:scale-[105%] cursor-pointer'>
-      <Image src={image} alt={name} className='w-full object-cover'/>
-      <div className='flex flex-col p-4 gap-2'>
+    <div className="w-full sm:h-[400px] flex flex-col border rounded-xl overflow-hidden transition hover:scale-[105%] cursor-pointer">
+      {image ? (
+        <Image
+          src={image}
+          alt={name}
+          className="w-full max-sm:h-[150px] sm:aspect-square object-cover"
+        />
+      ) : (
+        <div className="w-full max-sm:h-[150px] grid sm:aspect-square bg-gray-300"><p className="place-self-center text-white text-center">NO<br/>IMAGE</p></div>
+      )}
+      <div className="flex flex-col px-4 py-2 sm:py-4 gap-1 sm:gap-2">
         <p>{name}</p>
-        <p className='break-all line-clamp-3'>{description}</p>
+        <p className="break-all text-sm sm:text-base line-clamp-2 sm:line-clamp-3">{description}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
